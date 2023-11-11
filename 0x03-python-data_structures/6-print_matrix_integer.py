2d546567a2c17fb4adcc92b09962227c043cefb8
Bakkaaa <abubkr700@gmail.com>
2023-11-11 05:22:53 -0800

    B

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        str = " "
+        x = len(i)
+        for j in range(x):
+            if j == x - 1:
+                str = ""
+            print("{:d}".format(i[j]), end=str)
+        print("")
